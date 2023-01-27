d30b98e906efb5d6da65b5ef8bcd41ccd3111d5e
green <green@INGVARAS>
2023-01-27 18:32:01 +0200

    Add project files.

---
+++ Blackjack/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.plrNumber = new System.Windows.Forms.Label();
+            this.dealNumber = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.balanceText = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.betAmount = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.totalSeasonWinsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.betAmount)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(830, 553);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Balance:";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnHit.AutoSize = true;
+            this.btnHit.Enabled = false;
+            this.btnHit.Location = new System.Drawing.Point(391, 354);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 1;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStand.AutoSize = true;
+            this.btnStand.Enabled = false;
+            this.btnStand.Location = new System.Drawing.Point(472, 354);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(75, 23);
+            this.btnStand.TabIndex = 2;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(425, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Actions";
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStartGame.AutoSize = true;
+            this.btnStartGame.Location = new System.Drawing.Point(391, 383);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(156, 41);
+            this.btnStartGame.TabIndex = 4;
+            this.btnStartGame.Text = "START GAME";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(388, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DEALER:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(388, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "YOU:";
+            // 
+            // plrNumber
+            // 
+            this.plrNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.plrNumber.AutoSize = true;
+            this.plrNumber.Location = new System.Drawing.Point(391, 213);
+            this.plrNumber.Name = "plrNumber";
+            this.plrNumber.Size = new System.Drawing.Size(83, 13);
+            this.plrNumber.TabIndex = 7;
+            this.plrNumber.Text = "Player\'s Number";
+            this.plrNumber.Visible = false;
+            // 
+            // dealNumber
+            // 
+            this.dealNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dealNumber.AutoSize = true;
+            this.dealNumber.Location = new System.Drawing.Point(391, 62);
+            this.dealNumber.Name = "dealNumber";
+            this.dealNumber.Size = new System.Drawing.Size(85, 13);
+            this.dealNumber.TabIndex = 8;
+            this.dealNumber.Text = "Dealer\'s Number";
+            this.dealNumber.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(246, 519);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(443, 42);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "DEALER HITS SOFT 17";
+            // 
+            // balanceText
+            // 
+            this.balanceText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.balanceText.AutoSize = true;
+            this.balanceText.Location = new System.Drawing.Point(876, 553);
+            this.balanceText.Name = "balanceText";
+            this.balanceText.Size = new System.Drawing.Size(25, 13);
+            this.balanceText.TabIndex = 10;
+            this.balanceText.Text = "500";
+            this.balanceText.Click += new System.EventHandler(this.balanceText_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(440, 438);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Bet Amount";
+            // 
+            // betAmount
+            // 
+            this.betAmount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.betAmount.AutoSize = true;
+            this.betAmount.Location = new System.Drawing.Point(391, 454);
+            this.betAmount.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.betAmount.Name = "betAmount";
+            this.betAmount.Size = new System.Drawing.Size(156, 20);
+            this.betAmount.TabIndex = 13;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(96, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statsToolStripMenuItem
+            // 
+            this.statsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.totalSeasonWinsToolStripMenuItem});
+            this.statsToolStripMenuItem.Name = "statsToolStripMenuItem";
+            this.statsToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.statsToolStripMenuItem.Text = "Stats";
+            this.statsToolStripMenuItem.Click += new System.EventHandler(this.statsToolStripMenuItem_Click);
+            // 
+            // totalSeasonWinsToolStripMenuItem
+            // 
+            this.totalSeasonWinsToolStripMenuItem.Name = "totalSeasonWinsToolStripMenuItem";
+            this.totalSeasonWinsToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.totalSeasonWinsToolStripMenuItem.Text = "Total Session Profit";
+            this.totalSeasonWinsToolStripMenuItem.Click += new System.EventHandler(this.totalSeasonWinsToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToPlayToolStripMenuItem,
+            this.creditsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToPlayToolStripMenuItem
+            // 
+            this.howToPlayToolStripMenuItem.Name = "howToPlayToolStripMenuItem";
+            this.howToPlayToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.howToPlayToolStripMenuItem.Text = "How To Play?";
+            this.howToPlayToolStripMenuItem.Click += new System.EventHandler(this.howToPlayToolStripMenuItem_Click);
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            this.creditsToolStripMenuItem.Click += new System.EventHandler(this.creditsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(968, 570);
+            this.Controls.Add(this.betAmount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.balanceText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dealNumber);
+            this.Controls.Add(this.plrNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Blackjack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.betAmount)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label plrNumber;
+        private System.Windows.Forms.Label dealNumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label balanceText;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown betAmount;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToPlayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem statsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem totalSeasonWinsToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Blackjack/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Blackjack
+{
+    public partial class Form1 : Form
+    {
+        string profit = "0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(balanceText.Text) < Convert.ToInt32(Math.Round(betAmount.Value, 0)))
+            {
+                MessageBox.Show("Insufficient Balance");
+            }
+            else
+            {
+                balanceText.Text = (int.Parse(balanceText.Text) - Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+
+                btnStartGame.Visible = false;
+                betAmount.Enabled = false;
+                btnHit.Enabled = true;
+                dealNumber.Visible = true;
+                plrNumber.Visible = true;
+                btnStand.Enabled = true;
+                Random rnd = new Random();
+                dealNumber.Text = rnd.Next(1, 11).ToString();
+                int firstNumber = rnd.Next(1, 11);
+                int secondNumber = rnd.Next(1, 11);
+                plrNumber.Text = (firstNumber + secondNumber).ToString();
+            }
+
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            if (int.Parse(plrNumber.Text) <= 21)
+            {
+                plrNumber.Text = (int.Parse(plrNumber.Text) + rnd.Next(1, 11)).ToString();
+                if (int.Parse(plrNumber.Text) > 21)
+                {
+                    MessageBox.Show($"YOU LOSE. YOUR NUMBER : {plrNumber.Text},DEALER NUMBER : {dealNumber.Text}");
+                    profit = (int.Parse(profit) - Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+                    betAmount.Enabled = true;
+                    btnStartGame.Visible = true;
+                    btnHit.Enabled = false;
+                    dealNumber.Visible = false;
+                    dealNumber.Text = "0";
+                    plrNumber.Visible = false;
+                    plrNumber.Text = "0";
+                    btnStand.Enabled = false;
+                }
+            }
+
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            btnHit.Enabled = false;
+            bool restart = false;
+            while (!restart)
+            {
+                
+                if (int.Parse(dealNumber.Text) <= 16)
+                {
+                    
+                    dealNumber.Text = (int.Parse(dealNumber.Text) + rnd.Next(1, 11)).ToString();                    
+
+                }
+                if (int.Parse(dealNumber.Text) >= 17 && int.Parse(dealNumber.Text) <= 21)
+                {
+                    restart = true;
+                    if (int.Parse(dealNumber.Text) > int.Parse(plrNumber.Text))
+                    {
+                        MessageBox.Show($"You Lost! Dealer's Number : {dealNumber.Text}, Your Number : {plrNumber.Text}");
+                        profit = (int.Parse(profit) - Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+                    }
+                    else if (int.Parse(dealNumber.Text) < int.Parse(plrNumber.Text))
+                    {
+                        MessageBox.Show($"You Won! Dealer's Number : {dealNumber.Text}, Your Number : {plrNumber.Text}");
+                        balanceText.Text = (int.Parse(balanceText.Text) + Convert.ToInt32(Math.Round(betAmount.Value, 0)) * 2).ToString();
+                        profit = (int.Parse(profit) + Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+                    }
+                    else if (int.Parse(dealNumber.Text) == int.Parse(plrNumber.Text))
+                    {
+                        MessageBox.Show($"Tie! Dealer's Number : {dealNumber.Text}, Your Number : {plrNumber.Text}");
+                        balanceText.Text = (int.Parse(balanceText.Text) + Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+                    }
+                    betAmount.Enabled = true;
+                    btnStartGame.Visible = true;
+                    btnHit.Enabled = false;
+                    dealNumber.Visible = false;
+                    dealNumber.Text = "0";
+                    plrNumber.Visible = false;
+                    plrNumber.Text = "0";
+                    btnStand.Enabled = false;
+
+                }
+                if (int.Parse(dealNumber.Text) > 21)
+                {
+                    restart = true;
+                    MessageBox.Show($"You Won! Dealer Busted. Dealer's Number : {dealNumber.Text}, Your Number : {plrNumber.Text}");
+                    profit = (int.Parse(profit)+ Convert.ToInt32(Math.Round(betAmount.Value, 0))).ToString();
+                    balanceText.Text = (int.Parse(balanceText.Text) + Convert.ToInt32(Math.Round(betAmount.Value, 0)) * 2).ToString();
+                    betAmount.Enabled = true;
+                    btnStartGame.Visible = true;
+                    btnHit.Enabled = false;
+                    dealNumber.Visible = false;
+                    dealNumber.Text = "0";
+                    plrNumber.Visible = false;
+                    plrNumber.Text = "0";
+                    btnStand.Enabled = false;
+                }
+            }
+
+
+        }
+
+        private void balanceText_Click(object sender, EventArgs e)
+        {
+            balanceText.Text = (int.Parse(balanceText.Text) + 500).ToString();
+        }
+
+        private void howToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.");
+        }
+
+        private void creditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by Ingvaras");
+        }
+        private void totalSeasonWinsToolStripMenuItem_Click(object sender, EventArgs e) // Profit
+        {
+            MessageBox.Show($"{profit}");
+        }
+        private void statsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
